c2a7ee6b8409144b9718d9eab06ad014c55969bf
datadevda1 <datadevda1@gmail.com>
2024-05-28 14:10:18 -0400

    Reloaded updated Neovim config with hover and lsp messages disabled for cmdline plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -118,6 +118,18 @@ return {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
+    config = function()
+      require("noice").setup {
+        lsp = {
+          signature = {
+            enabled = false,
+          },
+          hover = {
+            enabled = false,
+          },
+        },
+      }
+    end,
   },
 
   --
