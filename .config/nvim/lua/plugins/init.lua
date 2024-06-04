1af896a04833d1d5d92da4e6f42e97b9e839ff2f
datadevda1 <datadevda1@gmail.com>
2024-06-04 19:37:28 -0400

    Added todo-comments.nvim to list of plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -189,7 +189,23 @@ return {
       },
     },
   },
-  --
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    config = function()
+      vim.keymap.set("n", "]t", function()
+        require("todo-comments").jump_next()
+      end, { desc = "Next todo comment" })
+
+      vim.keymap.set("n", "[t", function()
+        require("todo-comments").jump_prev()
+      end, { desc = "Previous todo comment" })
+
+      require("todo-comments").setup()
+    end,
+  }
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
